465a7831685e3b89b1a0e7eec629bdc4c8e86886
keith lesser <klesser@anaren.com>
2016-12-01 15:43:11 -0500

    Added state tracking so it wont turn off if its already off

--- driver/common/objects/tone_common.c
+++ driver/common/objects/tone_common.c
@@ -3,17 +3,29 @@
 #include "../gpio/gpio.h"
 #include "../pin_mapping.h"
 
+static bool toneOn;
+
 void TONE_Init()
 {
 	AIR_PWM_Enable(TONE_PWM_PIN);
+	toneOn = true;
+	TONE_Stop();
 }
 
 void TONE_Start(unsigned int note)
 {
-	AIR_PWM_SetDutyCycleAndFrequency(TONE_PWM_PIN, 50, (note/1000));
+	if(!toneOn)
+	{
+		AIR_PWM_SetDutyCycleAndFrequency(TONE_PWM_PIN, 50, (note/1000));
+		toneOn = true;
+	}
 }
 
 void TONE_Stop()
 {
-	AIR_PWM_SetDutyCycleAndFrequency(TONE_PWM_PIN, 0, 2000);
+	if(toneOn)
+	{
+		AIR_PWM_SetDutyCycleAndFrequency(TONE_PWM_PIN, 0, 2000);
+		toneOn = false;
+	}
 }
